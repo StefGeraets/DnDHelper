d7f738befa917069e8e1ee9e788ceb98dd894e78
Stef Geraets <stef@getnoticed.nl>
2019-10-15 17:01:42 +0200

    Remove joke

---
+++ precache-manifest.4aafd12c0753cdaf8eec25806c960fd4.js
@@ -0,0 +1,69 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c48f55e541ff1f2b78c",
+    "url": "bundle.css"
+  },
+  {
+    "revision": "3c48f55e541ff1f2b78c",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "420f6398cf0cee5cbda8db217e58cdf5",
+    "url": "favicon.png"
+  },
+  {
+    "url": "fonts/2acd74ecc050777d9e5d8df7fc4439b8.eot"
+  },
+  {
+    "url": "fonts/33aaad79235bb405b15b4a6b9a257cfa.woff2"
+  },
+  {
+    "url": "fonts/68b0e21139f22be61f55bba014320d3f.woff"
+  },
+  {
+    "url": "fonts/6939cf279137d993f276be3269f24dc7.woff"
+  },
+  {
+    "url": "fonts/a2ad308f137d2ada86ff54a4d7be9976.ttf"
+  },
+  {
+    "url": "fonts/d3998f382dead315a0c879e759d910e8.woff2"
+  },
+  {
+    "url": "fonts/e64c4438cba2dc6502becbc112296c3c.ttf"
+  },
+  {
+    "url": "fonts/fd0fa079d55b3415e339f2fe7709eac7.eot"
+  },
+  {
+    "url": "icon_128x128.6490a3e5da4a0efe340fbee13c1f43b8.png"
+  },
+  {
+    "url": "icon_192x192.6af2a332776850f6a6c3f97b8fc1f9c2.png"
+  },
+  {
+    "url": "icon_256x256.2003ee0edba5255aa534bf03b715e387.png"
+  },
+  {
+    "url": "icon_384x384.05b777285759c46797dfc23abc693288.png"
+  },
+  {
+    "url": "icon_512x512.cb924fbac2fe31fc0b16dc6f37dbf549.png"
+  },
+  {
+    "url": "icon_96x96.58c2801760c74aa113b74f2341b36d95.png"
+  },
+  {
+    "url": "images/39fb4a7fb52a3b712f247c0ea5e365a9.svg"
+  },
+  {
+    "url": "images/f7d05c95afe1c67fb7e66dc3b4c193e0.svg"
+  },
+  {
+    "revision": "3a762d18f3bc782bd27804086a180681",
+    "url": "index.html"
+  },
+  {
+    "url": "manifest.c661ce2614c01c22a2be2bc0ecf26387.json"
+  }
+]);
--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8da6a61c27a4ee33d88636b7607557b8.js"
+  "precache-manifest.4aafd12c0753cdaf8eec25806c960fd4.js"
 );
 
 workbox.core.skipWaiting();
